f0f5e970fe491586a5a256b3075d0775c2eaf81d
Andrew Walden <awalden0808@gmail.com>
2021-01-05 12:16:00 -0600

    Create album for song when song is added (#9)
    * Create album for song when song is added
    
    * Remove commented lines
    
    * Ensure this works for albums with multiple songs
    
    * Remove console logging

--- data-object/album.js
+++ data-object/album.js
@@ -1,5 +1,25 @@
 class Album {
-
+    fromMetadata(metadata) {
+        const common = metadata.common;
+        this.id = 0;
+        this.title = common.album ? common.album : '';
+        this.year = common.year ? common.year : 0;
+        this.artist = common.albumArtist ? common.albumArtist :
+            (common.artist ? common.artist : '');
+        this.genre = common.genre ? common.genre : '';
+        return this;
+    }
+    fromDB(row) {
+        if (row == undefined) {
+            return undefined;
+        }
+        this.id = row.album_id;
+        this.title = row.title;
+        this.year = row.year;
+        this.artist = row.artist;
+        this.genre = row.genre;
+        return this;
+    }
 };
 
 module.exports.Album = Album;
--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 require('./rest-api');
 const manager = require('./manager');
 
+const addSongs1 = new Promise((resolve, reject) => {
+    manager.addSongFromPath(__dirname + '/test-audio/007-nameless.m4a').then(() => {
+        manager.addSongFromPath(__dirname + '/test-audio/008-RERE.m4a').then(() => {
+            manager.addSongFromPath(__dirname + '/test-audio/test.mp3').then(() => {
+                resolve();
+            }).catch((err) => {
+                reject(err);
+            });
+        }).catch((err) => {
+            reject(err);
+        });
+    }).catch((err) => {
+        reject(err);
+    })
+});
 
-manager.addSongFromPath(__dirname + '/test-audio/008-RERE.m4a').then(() => {
-    manager.addSongFromPath(__dirname + '/test-audio/test.mp3').then(() => {
-        manager.getAllSongs().then((rows) => {
-            console.log(rows);
-        }).catch(() => {});
-    }).catch(() => {});
-}).catch(() => {});
+addSongs1.then(() => {
+    manager.getAllSongs().then((rows) => {
+        console.log(rows);
+    }).catch((err) => {
+        console.log(err);
+    });
+}).catch((err) => {
+    console.log(err);
+});
