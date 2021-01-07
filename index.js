e81bb09d2c56f2cf371fb71024f302923df4d67e
Wall-Dough <awalden0808@gmail.com>
2021-01-07 13:23:04 -0600

    Scan folders for media files

--- index.js
+++ index.js
@@ -1,19 +1,4 @@
 require('./data-access').init().then(() => {
     const manager = require('./manager');
     require('./rest-api');
-
-    manager.scanFoldersForMediaFiles().then(() => {
-        manager.getAllSongs().then((songs) => {
-            console.log(songs);
-        }).catch((err) => {
-            console.log(err);
-        });
-        manager.getAllAlbums().then((albums) => {
-            console.log(albums);
-        }).catch((err) => {
-            console.log(err);
-        });
-    }).catch((err) => {
-        console.log(err);
-    });
 });
