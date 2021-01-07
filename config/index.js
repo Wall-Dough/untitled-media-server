ff26c5005eced1770269e94f69185629a4c77c80
Andrew Walden <awalden0808@gmail.com>
2021-01-07 12:27:22 -0600

    Implement config (#32)

--- config/index.js
+++ config/index.js
@@ -1,19 +1,67 @@
 const util = require('../util');
 const fs = require('fs');
 
-const CONFIG_PATH = '../config.json';
+const CONFIG_PATH = util.relativePath('../server-config.json');
+
+// Singleton for config
+let config = undefined;
 
 const getConfig = () => {
-    const configPath = util.relativePath(CONFIG_PATH);
-    if (!fs.existsSync(configPath)) {
-        const configText = `{
-            // An array of absolute paths of folders to index
-            folders: []
-        }`;
-        fs.writeFileSync(configPath, configText, 'utf8');
+    if (config != undefined) {
+        return config;
     }
-    const configText = fs.readFileSync(configPath, 'utf8');
-    return JSON.parse(configText);
+    if (!fs.existsSync(CONFIG_PATH)) {
+        config = {
+            folders: []
+        };
+        fs.writeFileSync(CONFIG_PATH, JSON.stringify(config), 'utf8');
+        return config;
+    }
+    const configText = fs.readFileSync(CONFIG_PATH, 'utf8');
+    config = JSON.parse(configText)
+    return config;
 };
 
-module.exports.getConfig = getConfig;
+const get = (key) => {
+    const config = getConfig();
+    return config[key];
+};
+
+const set = (key, value) => {
+    const config = getConfig();
+    config[key] = value;
+    fs.writeFileSync(CONFIG_PATH, JSON.stringify(config));
+};
+
+const unset = (key) => {
+    const config = getConfig();
+    delete config[key];
+    fs.writeFileSync(CONFIG_PATH, JSON.stringify(config));
+};
+
+const getFolders = () => {
+    const folders = get('folders');
+    return folders == undefined ? [] : folders;
+};
+
+const addFolder = (folderName, folderPath) => {
+    let folders = getFolders();
+    folders.push({
+        name: folderName,
+        path: folderPath
+    });
+    set('folders', folders);
+};
+
+const removeFolder = (i) => {
+    let folders = getFolders();
+    folders.splice(i, 1);
+    set('folders', folders);
+};
+
+module.exports.get = get;
+module.exports.set = set;
+module.exports.unset = unset;
+module.exports.getFolders = getFolders;
+module.exports.addFolder = addFolder;
+module.exports.removeFolder = removeFolder;
